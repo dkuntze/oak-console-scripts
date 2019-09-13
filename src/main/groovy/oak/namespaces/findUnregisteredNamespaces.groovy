5df6290a01ebdc6ca940e1c6bbe8589e600d42b2
blackfor <32678777+blackfor@users.noreply.github.com>
2019-09-13 20:18:09 +0100

    Create findUnregisteredNamespaces.groovy

---
+++ src/main/groovy/oak/namespaces/findUnregisteredNamespaces.groovy
@@ -0,0 +1,57 @@
+import org.apache.jackrabbit.oak.spi.state.*;
+import org.apache.jackrabbit.oak.api.*;
+import java.util.*;
+import org.apache.jackrabbit.util.*;
+
+class UnregisteredNamespaceChecker {
+	def NodeStore nodeStore;
+	def nsCache = [:];
+	def missingPrefixes = [] as Set;
+	def checkedNodeCount = 0 as long;
+	
+	def check(){
+		def timeStarted = new Date().getTime();
+		
+		def ns = nodeStore.getRoot().getChildNode("jcr:system").getChildNode("rep:namespaces");
+		
+		// Populate the namespace cache
+		ns.getProperties().each { prop ->
+			if(prop.getName().indexOf(':')<0){
+				nsCache[prop.getName()] = prop.getValue(Type.STRING);
+			}
+		}
+		
+		// println("Current namespaces "+nsCache);
+		
+		// Walk the tree and check
+		checkNode(nodeStore.getRoot().getChildNode("content"));
+
+		def timeTaken = new Date().getTime() - timeStarted;
+		
+		println("Checked $checkedNodeCount nodes in ${timeTaken}ms, found ${missingPrefixes.size()} unregistered prefixes: ${missingPrefixes}");
+	}
+
+	def checkNode(ns){
+	
+		// Check local properties
+		ns.getProperties().each { prop ->
+			if(prop.getName().indexOf(':')>2){
+				def prefix = Text.getNamespacePrefix(prop.getName());
+
+				if(nsCache[prefix]==null && !missingPrefixes.contains(prefix)){
+					missingPrefixes.add(prefix);
+					println("Found missing prefix at "+prop.getName());
+				}
+			}	
+		}
+		++checkedNodeCount;
+			
+		// Check child nodes
+		ns.getChildNodeEntries().each { cne ->
+			checkNode(cne.getNodeState());	
+		}
+	}
+	
+}
+	
+new UnregisteredNamespaceChecker(nodeStore: session.store).check()
