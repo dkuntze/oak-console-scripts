a70d162b0d5101b4a994c00bd8054949701af13e
blackfor <32678777+blackfor@users.noreply.github.com>
2019-12-12 13:18:25 +0000

    Update UnprocessedAssetsFinder.groovy

--- src/main/groovy/assets/UnprocessedAssetsFinder.groovy
+++ src/main/groovy/assets/UnprocessedAssetsFinder.groovy
@@ -8,47 +8,99 @@ class UnprocessedAssetsFinder {
 
 	def checkedAssetCount = 0 as long;
 	def unprocessedAssets = 0 as long;
+	def processingAssets = 0 as long;
+	def scene7MissingAssets = 0 as long;
+	def scene7UnpublishedAssets = 0 as long;
 	
 	def check(){
 		def timeStarted = new Date().getTime();
 		
-		
 		// Walk the tree and check
-		traverseDAM(nodeStore.getRoot().getChildNode("content").getChildNode("dam"), "/content/dam");
+		traverseDAM(nodeStore.getRoot().getChildNode("content").getChildNode("dam"), "/content/dam", false);
 
 		def timeTaken = new Date().getTime() - timeStarted;
 		
 		println("Checked $checkedAssetCount assets in ${timeTaken}ms");
-		println("Found $unprocessedAssets 'processing' assets");
+		println("Found $unprocessedAssets 'unprocessed' assets");
+		println("Found $processingAssets 'processing' assets");
+		println("Found $scene7MissingAssets assets missing from Scene7");
+		println("Found $scene7UnpublishedAssets assets unpublished in Scene7");
+	}
+	
+	def getLocalS7Required(ns){
+		def answer = null;
+		
+		if(ns.hasProperty("cq:conf")){
+			answer = "include";
+		} else if ("include".equals(ns.getString("s7-sync-mode"))){
+			answer = "include";
+		} else if ("exclude".equals(ns.getString("s7-sync-mode"))){
+			answer = "exclude";
+		} else if (ns.hasChildNode("jcr:content")){
+			answer = getLocalS7Required(ns.getChildNode("jcr:content"));
+		}
+	
+		return answer;
 	}
 
-	def traverseDAM(ns, basePath){
+	def traverseDAM(ns, basePath, scene7Required){
 		
 		def nodeType = ns.getProperty("jcr:primaryType").getValue(org.apache.jackrabbit.oak.api.Type.NAME);
 
 		if(nodeType.equals("dam:Asset")){
+			boolean unprocessed = false;
+			boolean processing = false;
+			boolean scene7Missing = false;
+			boolean scene7Unpublished = false;
+			
+			def createdDate = ns.getProperty("jcr:created").getValue(org.apache.jackrabbit.oak.api.Type.DATE);
 			
 			if(ns.hasChildNode("jcr:content")){
-				def assetState = ns.getChildNode("jcr:content").getString("dam:assetState");
-				if("processing".equals(assetState)){
-					println("Found asset : $basePath");
+				def contentNode = ns.getChildNode("jcr:content");
+				
+				def assetState = contentNode.getString("dam:assetState");
+				
+				if(!"processed".equals(assetState)){
+					unprocessed = true;
 					++unprocessedAssets;
 				}
+				
+				if("processing".equals(assetState)){
+					processing = true;
+					++processingAssets;
+				}
+				
+				if(scene7Required && contentNode.hasChildNode("metadata")){
+					def metadataNode = contentNode.getChildNode("metadata");
+					
+					if(!metadataNode.hasProperty("dam:scene7File")){
+						scene7Missing = true;
+						++scene7MissingAssets;
+					} else if(!"PublishComplete".equals(metadataNode.getString("dam:scene7FileStatus"))){
+						scene7Unpublished = true;
+						++scene7UnpublishedAssets;
+					} 	
+				}
+				
+				if(unprocessed || processing || scene7Missing || scene7Unpublished){
+					println("$basePath,$createdDate,$unprocessed,$processing,$scene7Missing,$scene7Unpublished");
+				}
 			
-			} else {
-				println("Seen asset without content node $basePath");
-			}
+			} 
 			
 			++checkedAssetCount;
-			if(checkedAssetCount % 1000 == 0){
-				println("Checked $checkedAssetCount assets");	
+		} else if(nodeType.equals("sling:OrderedFolder") || nodeType.equals("sling:Folder")){
+		
+			def localS7Required = getLocalS7Required(ns);
+			if(scene7Required && ("exclude".equals(localS7Required))){
+				scene7Required = false;
+			} else if ("include".equals(localS7Required)){
+				scene7Required = true;
 			}
 			
-		} else if(nodeType.equals("sling:OrderedFolder") || nodeType.equals("sling:Folder")){
-
 			// Check child nodes
 			ns.getChildNodeEntries().each { cne ->
-				traverseDAM(cne.getNodeState(),basePath+"/"+cne.getName());	
+				traverseDAM(cne.getNodeState(),basePath+"/"+cne.getName(), scene7Required);	
 			}
 
 		}
